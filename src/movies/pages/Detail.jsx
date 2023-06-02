03f7d6c974e2e39aed8bbb9165439ff8b7f8efba
Nguyen Thanh Trieu <nguyenthanhtrieu90@gmail.com>
2023-06-02 21:29:29 +0700

    update code

--- src/movies/pages/Detail.jsx
+++ src/movies/pages/Detail.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react-refresh/only-export-components */
 import React, { useState, useEffect } from "react";
 import LayoutMovies from "../components/Layout";
-import { Row, Col, Image, Skeleton } from "antd";
+import { Row, Col, Image, Skeleton, Button } from "antd";
 import { useParams } from "react-router-dom";
 import { api } from "../services/api";
 import { helpers } from "../helpers/index";
+import YouTube from 'react-youtube';
+/*
+import ModalVideo from 'react-modal-video';
+import 'react-modal-video/css/modal-video.min.css';
+*/
 
 // eslint-disable-next-line react-refresh/only-export-components
 const DetailMovies = () => {
@@ -12,6 +17,18 @@ const DetailMovies = () => {
     const [loading, setLoading] = useState(true);
     const [movie, setMovie] = useState({});
     const [error, setError] = useState(null);
+    //const [isOpen, setOpen] = useState(false);
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+    };
+    const onPlayerReady = (event) => {
+        event.target.pauseVideo();
+    }
 
     useEffect(() => {
         const getData = async () => {
@@ -48,13 +65,18 @@ const DetailMovies = () => {
             </LayoutMovies>
         )
     }
+
     return (
         <LayoutMovies
             level1="Trang chu"
             level2="Chi tiet"
             level3={slug}
         >
-            <Row>
+            <Row style={{ 
+                backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize:'cover'
+            }}>
                 <Col span={24}>
                     <h4> Chi  tiet bo phim </h4>
                     <Row>
@@ -68,8 +90,54 @@ const DetailMovies = () => {
                             <div style={{ padding: '10px' }}>
                                 <h3>{movie.title}</h3>
                                 <p>{movie.overview}</p>
+                                {/*
+                                <div style={{ 
+                                    display: 'flex',
+                                    flexWrap: 'wrap',
+                                    justifyContent: 'space-around'
+                                }}>
+                                {
+                                    movie['videos']['results'].map((item,index) => (
+                                        <p key={index}>
+                                            <ModalVideo
+                                                channel='youtube'
+                                                autoplay
+                                                isOpen={isOpen}
+                                                videoId={item.key}
+                                                onClose={() => setOpen(false)}
+                                            />
+                                            <Button
+                                                type="primary"
+                                                onClick={()=> setOpen(true)}
+                                            >Trailer {index + 1}</Button>
+                                        </p>
+                                    ))
+                                }
+                                </div>
+                                */}
+                                {
+                                    movie['videos']['results'].map((item,index) => (
+                                        <div key={index} style={{ marginBottom: '5px' }}>
+                                            <YouTube
+                                                videoId={item.key}
+                                                opts={opts}
+                                                onReady={onPlayerReady}
+                                            />
+                                        </div>
+                                    ))
+                                }
                             </div>
                         </Col>
+                        {/* <Col span={8} style={{ padding: '10px' }}>
+                            <h5> Images movie</h5>
+                            {
+                                movie['images']['backdrops'].map((item,index) => (
+                                    <div key={index} style={{ marginBottom: '5px' }}>
+                                        <Image src={`https://image.tmdb.org/t/p/w300${item.file_path}`} />
+                                    </div>
+                                ) )
+                            }
+                        </Col> */}
                     </Row>
                 </Col>
             </Row>
--- src/movies/pages/Upcoming.jsx
+++ src/movies/pages/Upcoming.jsx
@@ -1,10 +1,92 @@
 /* eslint-disable react-refresh/only-export-components */
-import React from "react";
+import React, { useState } from "react";
 import LayoutMovies from "../components/Layout";
-import { Row, Col } from "antd";
+import { Row, Col, DatePicker, Skeleton } from "antd";
+import moment from 'moment';
+import { api } from "../services/api";
+import { helpers } from "../helpers/index";
+import ListMovies from "../components/ListMovies";
+import PaginationMovies from "../components/Pagination";
+
+const { RangePicker } = DatePicker;
 
 // eslint-disable-next-line react-refresh/only-export-components
 const UpcomingMovies = () => {
+    const [loading, setLoading] = useState(false);
+    const [dataMovies, setDataMovies] = useState([]);
+    const [errors, setErrors] = useState(null);
+    const [page, setPage] = useState(1);
+    const [totalPage, setTotalPage] = useState(0);
+    const [totalResult, setTotalResult] = useState(0);
+    const [sDate, setStartDate] = useState(null);
+    const [eDate,setEndDate] = useState(null);
+
+    const changeDate = async (date, dateString) => {
+        setLoading(true);
+        const [startDate, endDate] = dateString;
+        setStartDate(startDate);
+        setEndDate(endDate);
+        const data = await api.getDataMoviesByDate(startDate, endDate, page);
+        if(!helpers.isEmptyObject(data)){
+            // co du lieu
+            setDataMovies(data['results'])
+            if(page === 1){
+                setTotalPage(data['total_pages']);
+                setTotalResult(data['total_results']);
+            }
+            setErrors(null);
+        } else {
+            // khong co du lieu
+            setErrors({
+                code: 404,
+                mess: 'Not found data'
+            })
+        }
+        setLoading(false);
+    }
+
+    const changePageMovies = (p) => {
+        if( p >= 1 && p <= totalPage){
+            // cap nhat lai state page
+            // p : so trang ma nguoi dung bam o giao dien
+            setPage(p);
+            const stringDate = [sDate, eDate];
+            changeDate(null, stringDate);
+        }
+    }
+
+    if(loading){
+        return (
+            <LayoutMovies
+                level1="Trang chu"
+                level2="Danh sach"
+                level3="Phim sap ra rap"
+            >
+                <Row>
+                    <Col span={24}>
+                        <Skeleton active />
+                    </Col>
+                </Row>
+            </LayoutMovies>
+        )
+    }
+
+    if(errors !== null){
+        return (
+            <LayoutMovies
+                level1="Trang chu"
+                level2="Danh sach"
+                level3="Phim sap ra rap"
+            >
+                <Row>
+                    <Col span={24}>
+                        <h3>{errors.mess}</h3>
+                    </Col>
+                </Row>
+            </LayoutMovies>
+        )
+    }
+
     return (
         <LayoutMovies
             level1="Trang chu"
@@ -14,6 +96,31 @@ const UpcomingMovies = () => {
             <Row>
                 <Col span={24}>
                     <h4> Phim sap trinh chieu</h4>
+                    <Row>
+                        <Col span={24}>
+                            <RangePicker
+                                defaultValue={moment()}
+                                format={"YYYY-MM-DD"}
+                                disabledDate={current => {
+                                    return current && current < moment().endOf('day');
+                                }}
+                                onChange={(d, dt) => changeDate(d ,dt)}
+                            />
+                            <br/><br/>
+                            <ListMovies
+                                movies={dataMovies}
+                            />
+                            {
+                                dataMovies.length > 0 
+                                &&
+                                <PaginationMovies
+                                    current={page}
+                                    total={totalResult}
+                                    changePage={changePageMovies}
+                                />
+                            }
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </LayoutMovies>
