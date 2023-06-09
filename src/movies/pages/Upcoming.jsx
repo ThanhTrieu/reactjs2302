a330d6141dfd2ec5481834a2736d8c52f1fed2f4
Nguyen Thanh Trieu <nguyenthanhtrieu90@gmail.com>
2023-06-09 19:18:16 +0700

    update code

--- src/movies/pages/Upcoming.jsx
+++ src/movies/pages/Upcoming.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LayoutMovies from "../components/Layout";
 import { Row, Col, DatePicker, Skeleton } from "antd";
 import moment from 'moment';
@@ -12,37 +12,52 @@ const { RangePicker } = DatePicker;
 
 // eslint-disable-next-line react-refresh/only-export-components
 const UpcomingMovies = () => {
+    let date  = new Date();
+    let day   = date.getDate();
+    day       = day < 10 ? `0${day}` : day;
+    let month = date.getMonth()+1;
+    month     = month < 10 ? `0${month}` : month;
+    let year  = date.getFullYear();
+    let currentDate = `${year}-${month}-${day}`;
+    let nextDay   = moment(currentDate,"YYYY-MM-DD").add(1,'days').format("YYYY-MM-DD");
+    let nextMonth = moment(currentDate,"YYYY-MM-DD").add(7,'days').format("YYYY-MM-DD");
+
+
     const [loading, setLoading] = useState(false);
     const [dataMovies, setDataMovies] = useState([]);
     const [errors, setErrors] = useState(null);
     const [page, setPage] = useState(1);
     const [totalPage, setTotalPage] = useState(0);
     const [totalResult, setTotalResult] = useState(0);
-    const [sDate, setStartDate] = useState(null);
-    const [eDate,setEndDate] = useState(null);
+    const [sDate, setStartDate] = useState(nextDay);
+    const [eDate,setEndDate]    = useState(nextMonth);
 
-    const changeDate = async (date, dateString) => {
-        setLoading(true);
-        const [startDate, endDate] = dateString;
-        setStartDate(startDate);
-        setEndDate(endDate);
-        const data = await api.getDataMoviesByDate(startDate, endDate, page);
-        if(!helpers.isEmptyObject(data)){
-            // co du lieu
-            setDataMovies(data['results'])
-            if(page === 1){
+
+    useEffect(() => {
+        const getData = async () => {
+            setLoading(true);
+            const data = await api.getDataMoviesByDate(sDate, eDate, page);
+            if(!helpers.isEmptyObject(data)){
+                // co data
+                setDataMovies(data['results']);
                 setTotalPage(data['total_pages']);
                 setTotalResult(data['total_results']);
+                setErrors(null);
+            } else {
+                // khong co data
+                setErrors({code: 404,mess: 'Not found data'});
             }
-            setErrors(null);
-        } else {
-            // khong co du lieu
-            setErrors({
-                code: 404,
-                mess: 'Not found data'
-            })
+            setLoading(false);
         }
-        setLoading(false);
+        getData();
+    }, [sDate, eDate, page])
+
+    const changeDate = async (date, dateString) => {
+        const [startDate, endDate] = dateString;
+        // cap nhat lai start date va end date
+        // useEffect tu dong chay lai
+        setStartDate(startDate); // sDate
+        setEndDate(endDate); // eDate
     }
 
     const changePageMovies = (p) => {
@@ -50,25 +65,8 @@ const UpcomingMovies = () => {
             // cap nhat lai state page
             // p : so trang ma nguoi dung bam o giao dien
             setPage(p);
-            const stringDate = [sDate, eDate];
-            changeDate(null, stringDate);
+            // state page thay doi thi useEffect chay lai
         }
-    }
-
-    if(loading){
-        return (
-            <LayoutMovies
-                level1="Trang chu"
-                level2="Danh sach"
-                level3="Phim sap ra rap"
-            >
-                <Row>
-                    <Col span={24}>
-                        <Skeleton active />
-                    </Col>
-                </Row>
-            </LayoutMovies>
-        )
     }
 
     if(errors !== null){
@@ -99,7 +97,10 @@ const UpcomingMovies = () => {
                     <Row>
                         <Col span={24}>
                             <RangePicker
-                                defaultValue={moment()}
+                                defaultValue={[
+                                    moment(nextDay, "YYYY-MM-DD"),
+                                    moment(nextMonth, "YYYY-MM-DD")
+                                ]}
                                 format={"YYYY-MM-DD"}
                                 disabledDate={current => {
                                     return current && current < moment().endOf('day');
@@ -107,18 +108,24 @@ const UpcomingMovies = () => {
                                 onChange={(d, dt) => changeDate(d ,dt)}
                             />
                             <br/><br/>
-                            <ListMovies
-                                movies={dataMovies}
-                            />
-                            {
-                                dataMovies.length > 0 
-                                &&
-                                <PaginationMovies
-                                    current={page}
-                                    total={totalResult}
-                                    changePage={changePageMovies}
-                                />
-                            }
+                            { loading ? (
+                                <Skeleton active />
+                            ) : (
+                                <>
+                                    <ListMovies
+                                        movies={dataMovies}
+                                    />
+                                    {
+                                        dataMovies.length > 0 
+                                        &&
+                                        <PaginationMovies
+                                            current={page}
+                                            total={totalResult}
+                                            changePage={changePageMovies}
+                                        />
+                                    }
+                                </>
+                            )}
                         </Col>
                     </Row>
                 </Col>
